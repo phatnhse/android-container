33807931ba15b57366e4e021c8e62dc785c1cdb8
Hoang Phat Nguyen <hoangphat@inspectorio.com>
2019-12-03 16:59:16 +0700

    Run avd with bigger resolution for failed test

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 function wait_emulator_to_be_ready() {
   rm ~/.android/avd/test.avd/*.lock
   adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
-  emulator -avd test &
+  emulator -avd test -no-audio -no-boot-anim -no-window -accel on -gpu off -dpi-device 420 -skin 1440x2880 &
   boot_completed=false
   while [ "$boot_completed" == false ]; do
     status=$(adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
