3b88ce45de1984f9f590bde8c8216c4380d3de89
fastphat <11498224+fastphat@users.noreply.github.com>
2020-08-23 14:37:01 +0700

    Deprecate arm emulator (#28)

--- start.sh
+++ start.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-function wait_emulator_to_be_ready() {
+function check_kvm() {
   cpu_support_hardware_acceleration=$(grep -cw ".*\(vmx\|svm\).*" /proc/cpuinfo)
   kvm_support=$(kvm-ok)
-
-  emulator_name=${EMULATOR_NAME_ARM}
   if [ "$cpu_support_hardware_acceleration" != 0 ] && [ "$kvm_support" != *"NOT"* ]; then
-    emulator_name=${EMULATOR_NAME_x86}
+    echo 1
+  else
+    echo 0
   fi
+}
 
+function disable_animation() {
+  adb shell "settings put global window_animation_scale 0.0"
+  adb shell "settings put global transition_animation_scale 0.0"
+  adb shell "settings put global animator_duration_scale 0.0"
+}
+
+function wait_emulator_to_be_ready() {
   adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
-  emulator -avd "${emulator_name}" -verbose -no-boot-anim -no-window -gpu off &
+  emulator -avd "${EMULATOR_NAME_x86}" -verbose -no-boot-anim -wipe-data -no-snapshot -no-window -gpu off &
   boot_completed=false
   while [ "$boot_completed" == false ]; do
     status=$(adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
@@ -24,12 +32,15 @@ function wait_emulator_to_be_ready() {
   done
 }
 
-function disable_animation() {
-  adb shell "settings put global window_animation_scale 0.0"
-  adb shell "settings put global transition_animation_scale 0.0"
-  adb shell "settings put global animator_duration_scale 0.0"
+function start_emulator_if_possible() {
+  check_kvm=$(check_kvm)
+  if [ "$check_kvm" != "1" ]; then
+    echo "kvm nested virtualization is not supported"
+  else
+    wait_emulator_to_be_ready
+    sleep 1
+    disable_animation
+  fi
 }
 
-wait_emulator_to_be_ready
-sleep 1
-disable_animation
+start_emulator_if_possible
