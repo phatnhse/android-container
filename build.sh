f88ae7d6e64b968e11e6ae7d9011e1bc74d149cc
fastphat <11498224+fastphat@users.noreply.github.com>
2020-08-29 12:01:40 +0700

    Install additional Android SDK components (#32)
    * Update script to install missing android sdk packages
    
    * Update README

--- build.sh
+++
@@ -1,6 +0,0 @@
-docker build \
---build-arg GRADLE_VERSION=5.4.1 \
---build-arg ANDROID_API_LEVEL=28 \
---build-arg ANDROID_BUILD_TOOLS_LEVEL=28.0.3 \
---build-arg EMULATOR_NAME=test \
--t android-container:sunflower . 
